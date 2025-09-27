8b9a3f5d644b307e0d62198f2866fed9df5900e6
ajlgg <luthraa223@gmail.com>
2025-09-27 14:56:41 -0400

    routing to upload page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,20 +6,22 @@ import { Header } from "../components/Header";
 import Image from "next/image";
 
 export default function Home() {
+  const router = useRouter(); // initialize the router
+
   const handleGetStarted = () => {
-    console.log("Get Started clicked!");
+    router.push("/create"); // navigate to the upload page
   };
 
   return (
     <main className="relative flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-indigo-600 text-white px-4">
-    
       <div className="flex flex-col items-center justify-center text-center space-y-6 flex-grow">
-        <Header headerText="Welcome to TikTalk" subtext="Discover a new way to create, share, and comment. Our platform makes it simple and intuitive to get started."/>
+        <Header 
+          headerText="Welcome to TikTalk" 
+          subtext="Discover a new way to create, share, and comment. Our platform makes it simple and intuitive to get started."
+        />
         <Button onClick={handleGetStarted}>Get Started</Button>
 
-       
         <div className="flex gap-6 mt-10 items-center">
-          
           <div className="group relative w-64 h-64 rounded-2xl overflow-hidden shadow-lg cursor-pointer transform transition duration-300 hover:scale-105 hover:shadow-2xl">
             <Image
               src="/pdfimg.png"
@@ -32,7 +34,6 @@ export default function Home() {
             </div>
           </div>
 
-          
           <div className="group relative w-64 h-64 rounded-2xl overflow-hidden shadow-lg cursor-pointer transform transition duration-300 hover:scale-105 hover:shadow-2xl">
             <Image
               src="/mp4img.png"
@@ -45,7 +46,6 @@ export default function Home() {
             </div>
           </div>
 
-          
           <Image
             src="/blackarrow.png"
             alt="Arrow"
@@ -54,7 +54,6 @@ export default function Home() {
             className="animate-bounce"
           />
 
-       
           <div className="group relative w-64 h-64 rounded-2xl overflow-hidden shadow-lg cursor-pointer transform transition duration-300 hover:scale-105 hover:shadow-2xl">
             <Image
               src="/ttlogo.svg"
@@ -69,7 +68,6 @@ export default function Home() {
         </div>
       </div>
 
-    
       <div className="absolute left-6 bottom-6 w-72">
         <h1 className="text-6xl font-bold text-white mb-2">About Us</h1>
         <p className="text-sm min-w-screen text-white">
